6e6f2d8a7ad36b1a6bd1a78ae0b5cdbab0aad84c
Jason R. McNeil <jason@jasonrm.net>
2016-09-04 02:23:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Heil -
+
+## Requirements
+
+* [https://github.com/bitly/oauth2_proxy](oauth2_proxy)
+* [rabbitmq-auth-backend-http](https://github.com/rabbitmq/rabbitmq-auth-backend-http)
+
+| Request Path      | Response Status Code | Body                                       |
+|-------------------|----------------------|--------------------------------------------|
+| /auth/user        | 200                  | `allow administrator` if password matches, |
+| /auth/vhost       | 200                  | `allow`                                    |
+| /auth/resource    | 200                  | `allow`                                    |
+| _everything else_ | 404                  |                                            |
+
+## Usage
+
+`docker run -i -t -p 8080:8080 jasonrm/heli -basic-auth-password complexPassword`
+
+## Options
+
+```
+Usage of ./heli:
+  -basic-auth-password string
+        Must be the same as set on oauth2_proxy
+  -port int
+        port number (default 8080)
+```
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "crypto/subtle"
+    // "strconv"
+    "time"
+    "flag"
+    "os"
+)
+
+var expectedPassword = ""
+
+func main() {
+    portPtr := flag.Int("port", 8080, "port number")
+    passwordPtr := flag.String("basic-auth-password", "", "Must be the same as set on oauth2_proxy")
+    flag.Parse()
+
+    expectedPassword = *passwordPtr
+    if (expectedPassword == "") {
+        fmt.Fprintf(os.Stderr, "Missing required -basic-auth-password argument/flag\n")
+        os.Exit(2)
+    }
+
+    listenStr := fmt.Sprintf(":%d", *portPtr)
+
+    fmt.Println(time.Now().Format(time.RFC3339), "Listening On", listenStr);
+    http.HandleFunc("/auth/user", handleUser)
+    http.HandleFunc("/auth/vhost", handleVirtualHost)
+    http.HandleFunc("/auth/resource", handleResource)
+    http.HandleFunc("/", handleError)
+    http.ListenAndServe(listenStr, nil)
+}
+
+func handleUser(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    username := r.FormValue("username")
+    givenPassword := r.FormValue("password")
+
+    body := "deny"
+    if (subtle.ConstantTimeCompare([]byte(expectedPassword), []byte(givenPassword)) == 1) {
+        body = "allow administrator"
+    }
+
+    fmt.Fprint(w, body)
+    fmt.Println(time.Now().Format(time.RFC3339), r.Method, "User", body, username, r.UserAgent())
+}
+
+func handleVirtualHost(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    username := r.FormValue("username")
+    vhost := r.FormValue("vhost")
+    ip := r.FormValue("ip")
+
+    body := "deny"
+    if (len(username) > 0 && len(vhost) > 0 && len(ip) > 0) {
+        body = "allow"
+    }
+
+    fmt.Fprint(w, body)
+    fmt.Println(time.Now().Format(time.RFC3339), r.Method, "VirtualHost", body, username, vhost, ip, r.UserAgent())
+}
+
+func handleResource(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    username := r.FormValue("username")
+    vhost := r.FormValue("vhost")
+    resource := r.FormValue("resource")
+    name := r.FormValue("name")
+    permission := r.FormValue("permission")
+
+    body := "deny"
+    if (len(username) > 0 && len(vhost) > 0 && len(resource) > 0 && len(name) > 0 && len(permission) > 0) {
+        body = "allow"
+    }
+
+    fmt.Fprint(w, body)
+    fmt.Println(time.Now().Format(time.RFC3339), r.Method, "Resource", body, username, vhost, resource, name, permission, r.UserAgent())
+}
+
+func handleError(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+}
